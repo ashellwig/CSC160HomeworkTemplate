0afa3b0b089fdb38a6b0432208b2a87e218c1eb2
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-02-18 16:36:11 -0700

    initial commit

---
+++ _Scratch/src/GenFuncs.cpp
@@ -0,0 +1,8 @@
+#include "../include/GenFuncs.h"
+#include <iostream>
+
+int GenFuncs::pauseprompt() {
+  std::cout << "Press enter to continue" << std::endl;
+  std::cin.ignore();
+  return 0;
+}
---
+++ _Scratch/src/main.cpp
@@ -0,0 +1,12 @@
+#include "../include/GenFuncs.h"
+#include <iostream>
+
+int main() {
+  int num;
+  int sum;
+  std::cin >> num;
+
+
+  GenFuncs::pauseprompt();
+  return 0;
+}
